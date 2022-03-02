0a74c7cb76cf38664dcd992f6784a04cb6734a21
ckp-koiken <kentaro.koi0710@gmail.com>
2022-03-03 01:15:22 +0900

    wslconfig modify

--- mklink.bat
+++ mklink.bat
@@ -1,4 +1,5 @@
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles-windows\.vimrc"
 mklink %HOMEPATH%"\.gvimrc" %HOMEPATH%"\dotfiles-windows\.gvimrc"
 mklink %HOMEPATH%"\.latexmkrc" %HOMEPATH%"\dotfiles-windows\.latexmkrc"
+mklink %HOMEPATH%"\.wslconfig" %HOMEPATH%"\dotfiles-windows\.wslconfig"
 exit 0
