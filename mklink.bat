c0a01f2fdbdc03f1cf0bf59a10e1ce08ac22ab12
ckp-koiken <kentaro.koi0710@gmail.com>
2024-07-01 23:10:55 +0900

    wslconfigを削除

--- mklink.bat
+++ mklink.bat
@@ -1,5 +1,4 @@
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles-windows\.vimrc"
 mklink %HOMEPATH%"\.gvimrc" %HOMEPATH%"\dotfiles-windows\.gvimrc"
 mklink %HOMEPATH%"\.latexmkrc" %HOMEPATH%"\dotfiles-windows\.latexmkrc"
-mklink %HOMEPATH%"\.wslconfig" %HOMEPATH%"\dotfiles-windows\.wslconfig"
 exit 0
